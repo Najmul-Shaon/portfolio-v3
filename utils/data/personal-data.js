40fcf89ce7b1018037c243f4ec111e8ffb1db1f0
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-05-02 15:52:56 +0600

    push to main

---
+++ utils/data/personal-data.js
@@ -0,0 +1,20 @@
+export const personalData = {
+  name: "Najmul Hasan",
+  profile: "/profile_2.png",
+  designation: "Web Developer",
+  description:
+    "As a Web Developer, I transform ideas into digital masterpieces. With JavaScript at the core, I use React to weave powerful, responsive interfaces and MongoDB to store the heart of every project. Express and Node.js power the backend, ensuring flawless performance. I don’t just code—I craft stories through technology, building dynamic web experiences that resonate and engage. Let's go beyond development and create web experiences that people remember.",
+  email: "najmul.nh.shaon@gmail.com",
+  phone: "+88 01721 933810",
+  address: "Mohammadpur, Dhaka, Bangladesh - 1207",
+  github: "https://github.com/Najmul-Shaon",
+  facebook: "https://www.facebook.com/najmulshaonnhs/",
+  linkedIn: "https://www.linkedin.com/in/najmul-shaon",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  leetcode: "https://leetcode.com/said3812/",
+  hackerRank: "https://www.hackerrank.com/profile/najmul_nh_shaon",
+  devUsername: "said7388",
+  resume:
+    "https://drive.google.com/file/d/1rdkM2P60rZGpgudn_d6Ii0TkQd8zK04m/view?usp=sharing",
+};
